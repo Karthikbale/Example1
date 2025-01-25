69908a8b2edd464779e6137f33e8fdb11467877f
Karthikbale <emailkarthikbale2002@gmail.com>
2025-01-25 19:49:40 +0530

    changes102

--- Application .java
+++ Application .java
@@ -5,4 +5,17 @@
 <body> <h1> Java</h1>
 </body>
 </html>
+ 
+#Branch102 changes
 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Paragraph Example</title>
+</head>
+<body>
+    <p>This is a simple paragraph.</p>
+</body>
+</html>
+
+#branch102 ends
