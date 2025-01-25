e7655abe4f4e5b261b84d7b3e88e9b5b2107c94e
Karthikbale <emailkarthikbale2002@gmail.com>
2025-01-25 20:06:21 +0530

    1012 changes

--- Application .java
+++ Application .java
@@ -19,3 +19,20 @@
 </html>
 
 #Branch101 end
+
+#Branch1012 Changes
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ordered List</title>
+</head>
+<body>
+    <ol>
+        <li>Item 1</li>
+        <li>Item 2</li>
+    </ol>
+</body>
+</html>
+
+#Branch1012 ends
+
